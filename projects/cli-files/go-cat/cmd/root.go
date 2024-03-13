e2a42c048a3ca85a2223e1c3454a0e26df7d77fc
Baz <61154071+bazmurphy@users.noreply.github.com>
2024-03-13 20:31:43 +0000

    move main to root

--- projects/cli-files/go-cat/cmd/root.go
+++ projects/cli-files/go-cat/cmd/root.go
@@ -1,3 +1,59 @@
 package cmd
 
-func Execute() {}
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Execute() {
+
+	// if there is no argument given then error
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "no filename provided\n")
+		os.Exit(2)
+	}
+
+	// if there is an argument provided then save that argument as the filename
+	filename := os.Args[1]
+
+	// print out the filename to check
+	// fmt.Println("--- DEBUG filename", filename)
+
+	// check if the file exists
+	// fileInfo, err := os.Stat(filename)
+	_, err := os.Stat(filename)
+
+	// if the file does not exist then error
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "no such file or directory: %s\n", filename)
+		os.Exit(1)
+	}
+
+	// print out the fileInfo to check
+	// fmt.Println("--- DEBUG fileInfo", fileInfo)
+
+	// attempt to open the file
+	file, err := os.Open(filename)
+
+	// if the file cannot be opened then error
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot open the file: %v\n", err)
+		os.Exit(1)
+	}
+
+	// print out the file to check
+	// fmt.Println("--- DEBUG file", file)
+
+	// make sure to close the file at the end of the function
+	defer file.Close()
+
+	// make a scanner to read from the file
+	scanner := bufio.NewScanner(file)
+
+	// use the scanner to scan through the file, line by line, printing out each line
+	for scanner.Scan() {
+		fmt.Fprint(os.Stdout, scanner.Text(), "\n")
+	}
+
+}
--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -1,3 +1,66 @@
 package cmd
 
-func Execute() {}
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func Execute() {
+	// define a flag of type boolean with the specific properties (name, value, usage)
+	helpFlag := flag.Bool("h", false, "show help")
+
+	// parse the command line flags
+	flag.Parse()
+
+	// print out the value of helpFlag to check
+	// (!) deference the pointer with *
+	// fmt.Println("--- DEBUG helpFlag", *helpFlag)
+
+	// if the "-h" flag is provided
+	if *helpFlag {
+		fmt.Fprintf(os.Stdout, "go-ls help message\n")
+		return
+	}
+
+	// initialise/default the directory path to where go-ls was called from
+	directoryPath := "."
+
+	// if go-ls was called with an argument use that as the directory path instead
+	if len(os.Args) > 1 {
+		directoryPath = os.Args[1]
+	}
+
+	// print out the directory path to check
+	// fmt.Println("--- DEBUG directoryPath", directoryPath)
+
+	// get the absolute path of that directory
+	absolutePath, err := filepath.Abs(directoryPath)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "absolute path not found: %v\n", err)
+		os.Exit(2)
+	}
+
+	// print out the absolute path to check
+	// fmt.Println("--- DEBUG absolutePath", absolutePath)
+
+	// read from that directory
+	directory, err := os.ReadDir(absolutePath)
+
+	// if that directory doesn't exist
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "no such file or directory: %v\n", err)
+		os.Exit(2)
+	}
+
+	// print out the directory to check
+	// fmt.Println("--- DEBUG directory", directory)
+
+	// loop over the files/directories in the "directory" slice
+	for _, file := range directory {
+		// fmt.Println("--- DEBUG file", file)
+		fmt.Fprintf(os.Stdout, "%v\n", file.Name())
+	}
+}
