8f332d47d0764204ebea2f89c4142b5dc381c435
Baz Murphyts-node <bazmurphy@gmail.com>
2024-03-14 17:17:43 +0000

    add n flag, handle directories, change to ReadFile

--- projects/cli-files/go-cat/cmd/root.go
+++ projects/cli-files/go-cat/cmd/root.go
@@ -1,12 +1,18 @@
 package cmd
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func Execute() {
+	// define a flag of type boolean with the specific properties (name, value, usage)
+	numberFlag := flag.Bool("n", false, "number all output lines")
+
+	// parse the command line flags
+	flag.Parse()
 
 	// if there is no argument given then error
 	if len(os.Args) < 2 {
@@ -14,46 +20,88 @@ func Execute() {
 		os.Exit(2)
 	}
 
-	// if there is an argument provided then save that argument as the filename
-	filename := os.Args[1]
+	// create a variable to store the file name
+	var filename string
 
-	// print out the filename to check
-	// fmt.Println("--- DEBUG filename", filename)
+	if *numberFlag {
+		// if the -n flag is provided the filename will be the third value in the os.Args slice
+		filename = os.Args[2]
+	} else {
+		// if no flag is provided the filename will be the second value in the os.Args slice
+		filename = os.Args[1]
+	}
 
 	// check if the file exists
-	// fileInfo, err := os.Stat(filename)
-	_, err := os.Stat(filename)
+	fileInfo, err := os.Stat(filename)
+
+	if fileInfo.IsDir() {
+		fmt.Fprintf(os.Stderr, "%s : Is a directory\n", filename)
+		os.Exit(1)
+	}
 
 	// if the file does not exist then error
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "no such file or directory: %s\n", filename)
-		os.Exit(1)
+		os.Exit(2)
 	}
 
-	// print out the fileInfo to check
-	// fmt.Println("--- DEBUG fileInfo", fileInfo)
+	// attempt to read the file
+	file, err := os.ReadFile(filename)
 
-	// attempt to open the file
-	file, err := os.Open(filename)
-
-	// if the file cannot be opened then error
+	// if we cannot read the file then error
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "cannot open the file: %v\n", err)
-		os.Exit(1)
+		fmt.Fprintf(os.Stderr, "failed to read the file: %v\n", err)
+		os.Exit(2)
 	}
 
-	// print out the file to check
-	// fmt.Println("--- DEBUG file", file)
+	// convert the file into a string
+	fileContents := string(file)
 
-	// make sure to close the file at the end of the function
-	defer file.Close()
-
-	// make a scanner to read from the file
-	scanner := bufio.NewScanner(file)
-
-	// use the scanner to scan through the file, line by line, printing out each line
-	for scanner.Scan() {
-		fmt.Fprint(os.Stdout, scanner.Text(), "\n")
+	// if the -n flag was provided
+	if *numberFlag {
+		// split the string into lines using the newline delimiter
+		lines := strings.Split(fileContents, "\n")
+		// loop over the lines printing them with a line number prefix
+		for index, line := range lines {
+			fmt.Fprintf(os.Stdout, "%d  %s\n", index+1, line)
+		}
+		return
 	}
+
+	// directly print the file contents
+	fmt.Fprint(os.Stdout, fileContents)
+
+	// ----------
+
+	// bufio scanner method (currently left here for learning)
+
+	// // attempt to open the file
+	// file, err := os.Open(filename)
+
+	// // if the file cannot be opened then error
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "cannot open the file: %v\n", err)
+	// 	os.Exit(1)
+	// }
+
+	// // make sure to close the file at the end of the function
+	// defer file.Close()
+
+	// // make a scanner to read from the file
+	// scanner := bufio.NewScanner(file)
+
+	// // use the scanner to scan through the file, line by line, printing out each line
+
+	// // if there is a number flag then prefix it with a line number followed by two spaces
+	// if *numberFlag {
+	// 	for lineNumber := 1; scanner.Scan(); lineNumber++ {
+	// 		fmt.Fprint(os.Stdout, lineNumber, "  ", scanner.Text(), "\n")
+	// 	}
+	// 	return
+	// }
+
+	// for scanner.Scan() {
+	// 	fmt.Fprint(os.Stdout, scanner.Text(), "\n")
+	// }
 
 }
