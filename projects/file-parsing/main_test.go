0faed1e3afb11429c9846acb44500d89f3d35cb7
Baz Murphyts-node <bazmurphy@gmail.com>
2024-03-17 12:50:35 +0000

    continue adding tests

--- projects/file-parsing/main_test.go
+++ projects/file-parsing/main_test.go
@@ -7,37 +7,76 @@ import (
 
 func TestParseJSON(t *testing.T) {
 	t.Run("valid json", func(t *testing.T) {
-		// create a valid json "file"
-		validJSON := []byte(`[{"name":"playerOne","high_score":50},{"name":"playerTwo","high_score":0},{"name":"playerThree","high_score":25}]`)
+		validJSON := []byte(
+			`[
+				{"name":"playerOne","high_score":50},
+				{"name":"playerTwo","high_score":0},
+				{"name":"playerThree","high_score":25}
+			]`)
 
-		// initialise an empty slice of structs (players)
 		var dataSlice []Player
 
-		// try to parse the json
 		err := parseJSON(validJSON, &dataSlice)
 
-		// there should be no error
 		if err != nil {
 			t.Errorf("got %v | want no error", err)
 		}
 
-		// check the number of structs (players) in the data slice
 		if len(dataSlice) != 3 {
 			t.Errorf("got %d players | want %d players", len(dataSlice), 3)
 		}
 	})
 
-	t.Run("invalid json - one player", func(t *testing.T) {
-		// create an invalid json "file"
-		invalidJSON := []byte(`[{"name":,"high_score":50}`)
+	t.Run("invalid json", func(t *testing.T) {
+		invalidJSON := []byte(
+			`[{"name":"","high_score":50}`)
 
-		// initialise an empty slice of structs (players)
 		var dataSlice []Player
 
-		// try to parse the json
 		err := parseJSON(invalidJSON, &dataSlice)
 
 		wantErr := errors.New("[3] could not parse any data in JSON format")
+
+		if err == nil {
+			t.Errorf("got no error | want %q", wantErr)
+		}
+
+		if len(dataSlice) != 0 {
+			t.Errorf("got %d | want %d", len(dataSlice), 0)
+		}
+	})
+}
+
+func TestParseRepeatedJSON(t *testing.T) {
+	t.Run("valid repeatedJSON", func(t *testing.T) {
+		validRepeatedJSON := []byte(
+			`{"name":"playerOne","high_score":50}
+			# Comment
+			{"name":"playerTwo","high_score":0}
+			# Comment # Comment
+			{"name":"playerThree","high_score":25}`)
+
+		var dataSlice []Player
+
+		err := parseRepeatedJSON(validRepeatedJSON, &dataSlice)
+
+		if err != nil {
+			t.Errorf("got %v | want no error", err)
+		}
+
+		if len(dataSlice) != 3 {
+			t.Errorf("got %d, want %d", len(dataSlice), 3)
+		}
+	})
+
+	t.Run("invalid repeatedJSON", func(t *testing.T) {
+		invalidRepeatedJSON := []byte(`{"name":"playerOne","high_score":50{"name":"playerTwo","high_score":}`)
+
+		var dataSlice []Player
+
+		err := parseRepeatedJSON(invalidRepeatedJSON, &dataSlice)
+
+		wantErr := errors.New("[3] could not parse any data in repeated JSON format")
 
 		if err == nil {
 			t.Errorf("got no error | want %q", wantErr)
