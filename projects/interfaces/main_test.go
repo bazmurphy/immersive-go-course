e4699fcbfb81c949293359cbb1bbd13678ec8c83
Baz Murphyts-node <bazmurphy@gmail.com>
2024-04-05 14:00:56 +0100

    adjust tests based on feedback

--- projects/interfaces/main_test.go
+++ projects/interfaces/main_test.go
@@ -9,95 +9,78 @@ import (
 
 func TestOurBytesBuffer(t *testing.T) {
 	t.Run("returns the same bytes it was created with", func(t *testing.T) {
-		// create a byte slice with bytes
-		want := []byte{1, 2, 3}
-
-		// create a new buffer instance with the bytes
-		// b := bytes.NewBuffer(want)
-		b := NewOurBuffer(want)
+		// create a new buffer instance with some initial bytes
+		ourBuffer := NewOurBuffer([]byte{1, 2, 3})
 
 		// get the bytes from the buffer using the Bytes() method
-		got := b.Bytes()
+		got := ourBuffer.Bytes()
+
+		expectedByteSlice := []byte{1, 2, 3}
 
 		// error if the bytes from the buffer and the expected bytes are not the same
-		if !bytes.Equal(got, want) {
-			t.Errorf("got %q | want %q", got, want)
+		if !bytes.Equal(got, expectedByteSlice) {
+			t.Errorf("error byte slices are not equal: got %q | expected %q", got, expectedByteSlice)
 		}
 	})
 
 	t.Run("returns both initial bytes and the extra bytes", func(t *testing.T) {
-		// create a byte slice with initial bytes
-		initialBytes := []byte{1, 2, 3}
-
-		// create a new buffer instance with the initial bytes
-		// b := bytes.NewBuffer(initialBytes)
-		b := NewOurBuffer(initialBytes)
-
-		// create a byte slice with extra bytes
-		extraBytes := []byte{4, 5, 6}
+		// create a new buffer instance with some initial bytes
+		ourBuffer := NewOurBuffer([]byte{1, 2, 3})
 
 		// write the extra bytes to the buffer
-		_, err := b.Write(extraBytes)
+		numberOfBytesWritten, err := ourBuffer.Write([]byte{4, 5, 6})
 
-		// (?) necessary to prevent the test panicking
 		if err != nil {
-			t.Errorf("error writing to the buffer: %q", err)
+			t.Errorf("error received but expected none: got %q | expected %v", err, nil)
 		}
+
+		expectedByteSlice := []byte{1, 2, 3, 4, 5, 6}
+		bytesWritten := []byte{4, 5, 6}
+		expectedNumberOfBytesWritten := len(bytesWritten)
 
 		// get the bytes from the buffer using the Bytes() method
-		got := b.Bytes()
-
-		// create a byte slice with the expected bytes
-		want := []byte{1, 2, 3, 4, 5, 6}
+		got := ourBuffer.Bytes()
 
 		// error if the bytes from the buffer and the expected bytes are not the same
-		if !bytes.Equal(got, want) {
-			t.Errorf("bytes are not equal: got %q | want %q", got, want)
+		if !bytes.Equal(got, expectedByteSlice) {
+			t.Errorf("error byte slices are not equal: got %q | expected %q", got, expectedByteSlice)
 		}
 
-		// error if the lengths of the bytes from the buffer and the expected bytes are not the same
-		if len(got) != len(want) {
-			t.Errorf("bytes slice lengths are not the same: got %q | want %q", len(got), len(want))
+		if numberOfBytesWritten != expectedNumberOfBytesWritten {
+			t.Errorf("error number of bytes written: got %d | expected %d", numberOfBytesWritten, expectedNumberOfBytesWritten)
 		}
 	})
 
 	t.Run("slice big enough to read all of the bytes in the buffer", func(t *testing.T) {
-		// create a byte slice with initial bytes
-		initialBytes := []byte{1, 2, 3}
-
-		// create a new buffer instance with the initial bytes
-		// b := bytes.NewBuffer(initialBytes)
-		b := NewOurBuffer(initialBytes)
+		// create a new buffer instance with some initial bytes
+		ourBuffer := NewOurBuffer([]byte{1, 2, 3})
 
 		// make a byte slice with a size of the buffer length
-		bytesStoreSlice := make([]byte, b.Len())
+		bytesStoreSlice := make([]byte, ourBuffer.Len())
 
 		// read from the buffer
-		numberOfBytesRead, err := b.Read(bytesStoreSlice)
+		numberOfBytesRead, err := ourBuffer.Read(bytesStoreSlice)
 
-		// (?) necessary to prevent the test panicking
+		initialBytes := []byte{1, 2, 3}
+
 		if err != nil {
-			t.Errorf("error reading from the buffer: %q", err)
-		}
-
-		// error if the number of bytes read is not the same as the length of the initial bytes
-		if numberOfBytesRead != len(initialBytes) {
-			t.Errorf("number of bytes read: got %d | want %d", numberOfBytesRead, len(initialBytes))
+			t.Errorf("error received but expected none: got %q | expected %v", err, nil)
 		}
 
 		// error if the byte slices are not the same
 		if !bytes.Equal(bytesStoreSlice, initialBytes) {
 			t.Errorf("byte slices are not equal: got %v | want %v", bytesStoreSlice, initialBytes)
 		}
+
+		// error if the number of bytes read is not the same as the length of the initial bytes
+		if numberOfBytesRead != len(initialBytes) {
+			t.Errorf("number of bytes read: got %d | want %d", numberOfBytesRead, len(initialBytes))
+		}
 	})
 
 	t.Run("slice smaller than all of the bytes in the buffer", func(t *testing.T) {
-		// create a byte slice with initial bytes
-		initialBytes := []byte{1, 2, 3, 4, 5, 6, 7}
-
-		// create a new buffer instance with the initial bytes
-		// b := bytes.NewBuffer(initialBytes)
-		b := NewOurBuffer(initialBytes)
+		// create a new buffer instance with some initial bytes
+		ourBuffer := NewOurBuffer([]byte{1, 2, 3, 4, 5, 6, 7})
 
 		// initialise some slices to keep track of:
 		var byteReadValueHistory [][]byte // the byte values read from the buffer each read
@@ -111,20 +94,18 @@ func TestOurBytesBuffer(t *testing.T) {
 
 			// read the buffer into the small slice
 			// (!) if there is not enough values to read into slice, the rest will remain nil values (from make) in this case 0
-			numberOfBytesRead, err := b.Read(bytesStoreSliceSmall)
+			numberOfBytesRead, err := ourBuffer.Read(bytesStoreSliceSmall)
 
 			//(!) EOF is the error returned by Read when no more input is available
 			// if no more input is available then store the rest in bytesRemaining and exit the loop
 			if err == io.EOF {
 				// b.Bytes() gives us the unread portion of the buffer
-				bytesRemaining = b.Bytes()
+				bytesRemaining = ourBuffer.Bytes()
 				break
 			}
 
-			// (?) necessary to prevent the test panicking
 			if err != nil {
-				t.Errorf("error reading from the buffer: %q", err)
-				break
+				t.Errorf("error received but expected none: got %q | expected %v", err, nil)
 			}
 
 			// append the values to the history slices
@@ -153,26 +134,23 @@ func TestOurBytesBuffer(t *testing.T) {
 }
 
 func TestFilteringPipe(t *testing.T) {
-	// define a struct to hold the test cases
 	type TestCase struct {
-		name  string
-		input string
-		want  string
+		name     string
+		input    string
+		expected string
 	}
 
-	// create a slice of test cases (of type TestCase)
 	testCases := []TestCase{
-		{name: "digits after equals", input: "start=1, end=10", want: "start=, end="},
-		{name: "digits after every word", input: "hello123 and456 goodbye789", want: "hello and goodbye"},
-		{name: "digits wrapping a word", input: "010101binary010101", want: "binary"},
-		{name: "digit/character pattern", input: "1x2y3z", want: "xyz"},
-		{name: "all digits", input: "010101", want: ""},
-		{name: "no digits", input: "abcdef", want: "abcdef"},
+		{name: "digits after equals", input: "start=1, end=10", expected: "start=, end="},
+		{name: "digits after every word", input: "hello123 and456 goodbye789", expected: "hello and goodbye"},
+		{name: "digits wrapping a word", input: "010101binary010101", expected: "binary"},
+		{name: "digit/character pattern", input: "1x2y3z", expected: "xyz"},
+		{name: "all digits", input: "010101", expected: ""},
+		{name: "no digits", input: "abcdef", expected: "abcdef"},
 	}
 
-	// loop over the test cases
-	for _, testCase := range testCases {
-		t.Run(testCase.name, func(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
 			// create a new bytes.Buffer (this implements the io.Writer interface)
 			someWriter := &bytes.Buffer{}
 
@@ -180,14 +158,14 @@ func TestFilteringPipe(t *testing.T) {
 			filteringPipe := NewFilteringPipe(someWriter)
 
 			// write the input to the filter pipe
-			filteringPipe.Write([]byte(testCase.input))
+			filteringPipe.Write([]byte(tc.input))
 
 			// get the output string from the buffer
 			got := someWriter.String()
 
 			// error if the output (got) doesn't match the expected output (want)
-			if got != testCase.want {
-				t.Errorf("got %v | want %v", got, testCase.want)
+			if got != tc.expected {
+				t.Errorf("got %v | expected %v", got, tc.expected)
 			}
 		})
 	}
