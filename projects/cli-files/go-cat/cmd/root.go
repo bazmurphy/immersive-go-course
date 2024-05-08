8d35f688cbdb1db818a945794219d45cf402e9bc
Baz Murphyts-node <bazmurphy@gmail.com>
2024-05-08 18:22:21 +0100

    initial go-cat tests

--- projects/cli-files/go-cat/cmd/root.go
+++ projects/cli-files/go-cat/cmd/root.go
@@ -4,20 +4,18 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 )
 
-func Execute() {
-	// define a flag of type boolean with the specific properties (name, value, usage)
-	numberFlag := flag.Bool("n", false, "number all output lines")
+var (
+	numberFlag = flag.Bool("n", false, "number all output lines")
+)
 
-	// parse the command line flags
+func Execute() {
 	flag.Parse()
 
 	// get the non-flag arguments
 	args := flag.Args()
-	// fmt.Println("--- DEBUG args", args)
 
 	// if there is no non-flag argument given then error
 	if len(args) == 0 {
@@ -25,53 +23,79 @@ func Execute() {
 		os.Exit(2)
 	}
 
-	// (!) this is very hardcoded... what about more than one file passed to go-cat(?)
-	// (!) what about non filename arguments(?)
-	filename := args[0]
-
-	// try to get the file information
-	fileInfo, err := os.Stat(filename)
-	// if we cannot get the file information then error
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: could not read the file/directory information: %s\n", filename)
-		os.Exit(2)
-	}
-
-	// if the file is a directory then warn the user
-	if fileInfo.IsDir() {
-		fmt.Fprintf(os.Stderr, "error: %s : Is a directory\n", filename)
-		os.Exit(1)
-	}
-
-	// open the file
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: could not open the file: %v\n", err)
-		os.Exit(2)
-	}
-	defer file.Close()
-
-	// use a buffered reader to read from the file
-	reader := bufio.NewReader(file)
-
-	lineNumber := 1
-
-	for {
-		line, err := reader.ReadString('\n')
-
+	for _, filename := range args {
+		// try to get the file information
+		fileInfo, err := os.Stat(filename)
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Fprintf(os.Stderr, "error: reading line from the file: %v\n", err)
-			os.Exit(2)
+			fmt.Fprintf(os.Stderr, "go-cat: %s: No such file or directory\n", filename)
+			continue
 		}
 
-		if *numberFlag {
-			fmt.Fprintf(os.Stdout, "%d\t%s", lineNumber, line)
-			lineNumber++
-		} else {
-			fmt.Fprint(os.Stdout, line)
+		// if the file is a directory then warn the user
+		if fileInfo.IsDir() {
+			fmt.Fprintf(os.Stderr, "go-cat: %s: Is a directory\n", filename)
+			continue
 		}
+
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "go-cat: %v: failed to open the file\n", file)
+			continue
+		}
+		defer file.Close()
+
+		lineNumber := 1
+
+		// -------------------- METHOD 1 bufio.NewScanner
+
+		// bufio.NewScanner() creates a new Scanner to read input from a specified io.Reader
+		scanner := bufio.NewScanner(file)
+
+		// Scan() advances the Scanner to the next token (default is a line)
+		// and returns true if there are more tokens to read
+		// and returns false when it reaches the end of the input or encounters an error
+		for scanner.Scan() {
+			// Text() returns the most recently scanned token as a string
+			// it should be called only after a successful call to Scan()
+			line := scanner.Text()
+
+			if *numberFlag {
+				fmt.Fprintf(os.Stdout, "%d\t%s", lineNumber, line)
+				lineNumber++
+			} else {
+				// need to somehow work out whether this has a newline or not at the end
+				// and don't add the \n if it doesn't
+				fmt.Fprintf(os.Stdout, "%s", line)
+			}
+		}
+
+		// Err() returns the first non-EOF error that was encountered by the scanner
+		err = scanner.Err()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "go-cat: %v: failed to read the file\n", file)
+		}
+
+		// -------------------- METHOD 2 bufio.NewReader
+
+		// reader := bufio.NewReader(file)
+
+		// // attempt to read the file
+		// for {
+		// 	line, err := reader.ReadString('\n')
+		// 	if err != nil {
+		// 		if err == io.EOF {
+		// 			break
+		// 		}
+		// 		fmt.Fprintf(os.Stderr, "go-cat: %v: failed to read line %d", file, lineNumber)
+		// 		continue
+		// 	}
+
+		// 	if *numberFlag {
+		// 		fmt.Fprintf(os.Stdout, "%d\t%s", lineNumber, line)
+		// 		lineNumber++
+		// 	} else {
+		// 		fmt.Fprint(os.Stdout, line)
+		// 	}
+		// }
 	}
 }
---
+++ projects/cli-files/go-cat/cmd/root_test.go
@@ -0,0 +1,176 @@
+package cmd
+
+import (
+	"flag"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestExecute(t *testing.T) {
+	testCases := []struct {
+		name             string
+		flags            []string
+		args             []string
+		testFiles        []string
+		testFileContents []string
+		expectedOutput   string
+		expectedError    error
+	}{
+		{
+			name:             "no flags, args: 1 file, with a single line, ending WITH a new line",
+			flags:            []string{},
+			args:             []string{"test-file.txt"},
+			testFiles:        []string{"test-file.txt"},
+			testFileContents: []string{"hello\n"},
+			expectedOutput:   "hello\n",
+		},
+		{
+			name:             "no flags, args: 1 file, with a single line, ending WITHOUT a new line",
+			flags:            []string{},
+			args:             []string{"test-file.txt"},
+			testFiles:        []string{"test-file.txt"},
+			testFileContents: []string{"hello"},
+			expectedOutput:   "hello",
+		},
+		// {
+		// 	name:             "-n flag, args: 1 file, with a single line, ending WITH a new line",
+		// 	flags:            []string{"n"},
+		// 	args:             []string{"test-file.txt"},
+		// 	testFiles:        []string{"test-file.txt"},
+		// 	testFileContents: []string{"hello\n"},
+		// 	expectedOutput:   "1\thello\n",
+		// },
+		// {
+		// 	name:             "-n flag, args: 1 file, with a single line, ending WITHOUT a new line",
+		// 	flags:            []string{"n"},
+		// 	args:             []string{"test-file.txt"},
+		// 	testFiles:        []string{"test-file.txt"},
+		// 	testFileContents: []string{"hello"},
+		// 	expectedOutput:   "1\thello",
+		// },
+		// {
+		// 	name:             "no flags, args: 1 file, with multiple lines, ending WITH a new line",
+		// 	flags:            []string{},
+		// 	args:             []string{"test-file.txt"},
+		// 	testFiles:        []string{"test-file.txt"},
+		// 	testFileContents: []string{"hello\nfrom the\ntest file\ngoodbye\n"},
+		// 	expectedOutput:   "hello\nfrom the\ntest file\ngoodbye\n",
+		// },
+		// {
+		// 	name:             "no flags, args: 1 file, with multiple lines, ending WITHOUT a new line",
+		// 	flags:            []string{},
+		// 	args:             []string{"test-file.txt"},
+		// 	testFiles:        []string{"test-file.txt"},
+		// 	testFileContents: []string{"hello\nfrom the\ntest file\ngoodbye"},
+		// 	expectedOutput:   "hello\nfrom the\ntest file\ngoodbye",
+		// },
+		// {
+		// 	name:             "-n flag, args: 1 file, with multiple lines, ending WITH a new line",
+		// 	flags:            []string{"n"},
+		// 	args:             []string{"test-file.txt"},
+		// 	testFiles:        []string{"test-file.txt"},
+		// 	testFileContents: []string{"hello\nfrom the\ntest file\ngoodbye\n"},
+		// 	expectedOutput:   "1\thello\n2\tfrom the\n3\ttest file\n4\tgoodbye\n",
+		// },
+		// {
+		// 	name:             "-n flag, args: 1 file, with multiple lines, ending WITHOUT a new line",
+		// 	flags:            []string{"n"},
+		// 	args:             []string{"test-file.txt"},
+		// 	testFiles:        []string{"test-file.txt"},
+		// 	testFileContents: []string{"hello\nfrom the\ntest file\ngoodbye"},
+		// 	expectedOutput:   "1\thello\n2\tfrom the\n3\ttest file\n4\tgoodbye",
+		// },
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			// get the original working directory
+			originalWorkingDirectory, err := os.Getwd()
+			if err != nil {
+				t.Fatalf("failed to store original working directory: %v", err)
+			}
+
+			// create a temporary directory to create the test files in
+			temporaryDirectory, err := os.MkdirTemp(".", "go-cat-temporary-directory")
+			if err != nil {
+				t.Fatalf("failed to create a temporary directory: %v", err)
+			}
+			defer os.RemoveAll(temporaryDirectory)
+
+			// create the test files
+			for index, testFile := range testCase.testFiles {
+				testFilePath := filepath.Join(temporaryDirectory, testFile)
+				err := os.WriteFile(testFilePath, []byte(testCase.testFileContents[index]), 0644)
+				if err != nil {
+					t.Fatalf("failed to create test file: %v", err)
+				}
+			}
+
+			// change to the temporary directory to run Execute()
+			err = os.Chdir(temporaryDirectory)
+			if err != nil {
+				t.Fatalf("failed to change to the temporary directory: %v", err)
+			}
+
+			defer func() {
+				// at the end of the test: change back to the original working directory
+				err = os.Chdir(originalWorkingDirectory)
+				if err != nil {
+					t.Fatalf("failed to change to the temporary directory: %v", err)
+				}
+			}()
+
+			// reset all the flags
+			flag.VisitAll(func(f *flag.Flag) {
+				f.Value.Set(f.DefValue)
+			})
+
+			// if there are flags set them
+			if len(testCase.flags) > 0 {
+				for _, testCaseFlag := range testCase.flags {
+					err := flag.Set(testCaseFlag, "true")
+					if err != nil {
+						t.Fatalf("failed to set the flag %s : %v", testCaseFlag, err)
+					}
+				}
+			}
+
+			// set the arguments
+			if len(testCase.args) > 0 {
+				os.Args = append(os.Args, testCase.args...)
+			}
+
+			// ---------- I found this difficult to fully grasp ----------
+
+			// store the original stdout
+			originalStdout := os.Stdout
+
+			// create a pipe to capture the output
+			pipeRead, pipeWrite, _ := os.Pipe()
+
+			// redirect stdout to the write end of the pipe
+			os.Stdout = pipeWrite
+
+			Execute()
+
+			// close the write end of the pipe
+			pipeWrite.Close()
+
+			// read the captured output from the read end of the pipe
+			pipeReadBytes, _ := io.ReadAll(pipeRead)
+
+			actualOutput := string(pipeReadBytes)
+
+			// at the end of the test: restore the original stdout
+			os.Stdout = originalStdout
+
+			// -----------------------------------------------------------
+
+			if actualOutput != testCase.expectedOutput {
+				t.Errorf("output: actual %v | expected %v", actualOutput, testCase.expectedOutput)
+			}
+		})
+	}
+}
