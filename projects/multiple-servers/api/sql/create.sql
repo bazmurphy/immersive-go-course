847bed582ed06e3c82702c031769f0a64ae626d6
Baz Murphy <bazmurphy@gmail.com>
2024-05-18 08:55:23 +0100

    docker database migration

--- projects/multiple-servers/api/sql/create.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS images
-(
-    id serial PRIMARY KEY,
-    title text NOT NULL,
-    url text NOT NULL,
-    alt_text text
-);
--- projects/multiple-servers/api/sql/select.sql
+++
@@ -1,2 +0,0 @@
-SELECT id, title, url, alt_text
-  FROM images;
