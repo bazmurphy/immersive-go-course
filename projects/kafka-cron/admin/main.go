2205388eb12ab60b0e8cd0e906a986be18731c11
Baz Murphy <bazmurphy@gmail.com>
2024-05-28 10:36:56 +0100

    refactor admin

--- projects/kafka-cron/admin/main.go
+++ projects/kafka-cron/admin/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"strings"
 	"time"
@@ -10,8 +11,6 @@ import (
 	"github.com/twmb/franz-go/pkg/kadm"
 	"github.com/twmb/franz-go/pkg/kgo"
 )
-
-// (!) this became repetitive after the cluster-a and cluster-b (Part 2)
 
 var (
 	clusterASeedsFlag     string
@@ -30,97 +29,132 @@ func main() {
 
 	flag.Parse()
 
-	// log.Println("DEBUG | clusterASeedsFlag:", clusterASeedsFlag, "clusterBSeedsFlag", clusterBSeedsFlag, "topicFlag:", topicFlag, "partitionsFlag:", partitionsFlag, "replicationFactorFlag:", replicationFactorFlag)
-
+	// TODO: handle the flag errors more specifically
 	if clusterASeedsFlag == "" || clusterBSeedsFlag == "" || topicFlag == "" || partitionsFlag == 0 || replicationFactorFlag == 0 {
 		flag.Usage()
 		log.Fatalf("error: missing or invalid flag values")
 	}
 
+	// --------------------------------------------
+
 	clusterASeeds := strings.Split(clusterASeedsFlag, ",")
 	clusterBSeeds := strings.Split(clusterBSeedsFlag, ",")
 
-	log.Printf("new kafka admin client starting...")
-
-	var clusterAAdminClient *kadm.Client
-	{
-		client, err := kgo.NewClient(
-			kgo.SeedBrokers(clusterASeeds...),
-		)
-		if err != nil {
-			log.Fatalf("error: failed to create new admin client: %v\n", err)
-		}
-		defer client.Close()
-
-		clusterAAdminClient = kadm.NewClient(client)
+	adminClientClusterA, err := createAdminClient(clusterASeeds)
+	if err != nil {
+		log.Fatalf("error: failed to create admin client for cluster-a: %v", err)
 	}
+	defer adminClientClusterA.Close()
 
-	var clusterBAdminClient *kadm.Client
-	{
-		client, err := kgo.NewClient(
-			kgo.SeedBrokers(clusterBSeeds...),
-		)
-		if err != nil {
-			log.Fatalf("error: failed to create new admin client: %v\n", err)
-		}
-		defer client.Close()
-
-		clusterBAdminClient = kadm.NewClient(client)
+	adminClientClusterB, err := createAdminClient(clusterBSeeds)
+	if err != nil {
+		log.Fatalf("error: failed to create admin client for cluster-b: %v", err)
 	}
+	defer adminClientClusterB.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	// --------------------------------------------
 
 	topic := topicFlag
 	partitions := int32(partitionsFlag)
 	replicationFactor := int16(replicationFactorFlag)
 	configs := make(map[string]*string)
 
-	_, err := clusterAAdminClient.DeleteTopic(ctx, topic)
-	if err != nil {
-		log.Printf("error: failed to delete topic %s: %v\n", topic, err)
-	} else {
-		log.Printf("success: topic '%s' deleted\n", topic)
-	}
+	retryTopic := topic + "-retry"
 
-	_, err = clusterBAdminClient.DeleteTopic(ctx, topic)
+	err = deleteTopics(adminClientClusterA, topic, retryTopic)
 	if err != nil {
-		log.Printf("error: failed to delete topic %s: %v\n", topic, err)
-	} else {
-		log.Printf("success: topic '%s' deleted\n", topic)
+		log.Fatalf("error: failed to delete the topics from cluster-a: %v", err)
+	}
+	err = deleteTopics(adminClientClusterB, topic, retryTopic)
+	if err != nil {
+		log.Fatalf("error: failed to delete the topics from cluster-b: %v", err)
 	}
 
 	time.Sleep(1 * time.Second)
 
-	_, err = clusterAAdminClient.CreateTopic(ctx, partitions, replicationFactor, configs, topic)
-	if err != nil {
-		log.Printf("error: failed to create topic %s: %v\n", topic, err)
-	} else {
-		log.Printf("success: topic '%s' created\n", topic)
-	}
+	// --------------------------------------------
 
-	_, err = clusterBAdminClient.CreateTopic(ctx, partitions, replicationFactor, configs, topic)
+	err = createTopics(adminClientClusterA, partitions, replicationFactor, configs, topic, retryTopic)
 	if err != nil {
-		log.Printf("error: failed to create topic %s: %v\n", topic, err)
-	} else {
-		log.Printf("success: topic '%s' created\n", topic)
+		log.Fatalf("error: failed to create the topics on cluster-a: %v", err)
+	}
+	err = createTopics(adminClientClusterB, partitions, replicationFactor, configs, topic, retryTopic)
+	if err != nil {
+		log.Fatalf("error: failed to create the topics on cluster-b: %v", err)
 	}
 
 	time.Sleep(1 * time.Second)
 
-	metadata, err := clusterAAdminClient.Metadata(ctx, topic)
+	// --------------------------------------------
+
+	err = getMetadata(adminClientClusterA, topic, retryTopic)
 	if err != nil {
-		log.Printf("error: failed to get metadata for topic %s: %v\n", topic, err)
-	} else {
-		PrintMetaData(metadata)
+		log.Fatalf("error: failed to get the metadata of cluster-a: %v", err)
+	}
+	err = getMetadata(adminClientClusterB, topic, retryTopic)
+	if err != nil {
+		log.Fatalf("error: failed to get the metadata of cluster-b: %v", err)
 	}
 
-	metadata, err = clusterBAdminClient.Metadata(ctx, topic)
+	// --------------------------------------------
+}
+
+func createAdminClient(seeds []string) (*kadm.Client, error) {
+	client, err := kgo.NewClient(
+		kgo.SeedBrokers(seeds...),
+	)
 	if err != nil {
-		log.Printf("error: failed to get metadata for topic %s: %v\n", topic, err)
-	} else {
-		PrintMetaData(metadata)
+		return nil, fmt.Errorf("failed to create new admin client: %w", err)
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = client.Ping(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping the cluster: %w", err)
+	}
+
+	return kadm.NewClient(client), nil
+}
+
+func deleteTopics(adminClient *kadm.Client, topics ...string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := adminClient.DeleteTopics(ctx, topics...)
+	if err != nil {
+		return fmt.Errorf("error: failed to delete topics: %w", err)
+	}
+
+	log.Printf("topics '%s' deleted\n", topics)
+	return nil
+}
+
+func createTopics(adminClient *kadm.Client, partitions int32, replicationFactor int16, configs map[string]*string, topics ...string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := adminClient.CreateTopics(ctx, partitions, replicationFactor, configs, topics...)
+	if err != nil {
+		return fmt.Errorf("error: failed to create topics: %w", err)
+	}
+
+	log.Printf("topics '%s' created\n", topics)
+	return nil
+}
+
+func getMetadata(adminClient *kadm.Client, topics ...string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	metadata, err := adminClient.Metadata(ctx, topics...)
+	if err != nil {
+		return fmt.Errorf("error: failed to get metadata: %w", err)
+	}
+
+	PrintMetaData(metadata)
+	return nil
 }
 
 func PrintMetaData(metadata kadm.Metadata) {
